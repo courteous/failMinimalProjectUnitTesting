e7f6a1d3df13ebe022bcd7076d6efc76edba4a12
Tito <konstanitn@hristov.org>
2018-01-25 16:39:17 +0100

    test

---
+++ tests/Hello2.ts
@@ -0,0 +1,9 @@
+export class Hellow2 {
+
+  constructor(){}
+
+  public sayHello() : string{
+      return "Hello World!";
+  }
+
+}
---
+++ tests/example.ts
@@ -0,0 +1,17 @@
+
+import { expect } from '../node_modules/chai/';
+import {mocha} from "../node_modules/mocha/";
+
+
+import { Hellow2 } from './Hello2';
+
+describe('Hello function', () => {
+
+  it('should return hello world', () => {
+    // const result = hello();
+    var classHello2 = new Hellow2();
+    const result = classHello2.sayHello();
+    expect(result).to.equal('Hello World!');
+  });
+
+});
